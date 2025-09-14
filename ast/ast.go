c9d7bf15c60432f990f7eaab9b7aba613359b661
Sebastian <sebslight@gmail.com>
2025-09-14 14:53:49 -0400

    feat: add infix expression and boolean literal parsing
    - Add InfixExpression and Boolean types to AST
    - Implement operator precedence system (LOWEST to CALL levels)
    - Add infix expression parsing for arithmetic (+, -, *, /)
    - Add comparison operators (>, <, ==, !=)
    - Support boolean literals (true/false) parsing
    - Add grouped expression support with parentheses
    - Extend tests for infix expressions and precedence
    - Add parser tracer utility for debugging

--- ast/ast.go
+++ ast/ast.go
@@ -138,3 +138,32 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString(")")
 	return out.String()
 }
+
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.TokenLiteral() }
